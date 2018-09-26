e578c43917ade9fb3d7b21cd3b0cf990e51fc5c1
andrewistiny <aleachman3619@myhta.org>
2018-09-26 12:11:39 -1000

    data complete

---
+++ data.js
@@ -0,0 +1,224 @@
+const data = [
+///----------------------------------------1
+{
+    artist: 'AJR',
+    genre: 'Pop',
+    song: 'Bud Like You',
+    image: "https://i.ytimg.com/vi/3qce2Z4nlVs/maxresdefault.jpg"
+  },
+  {
+      artist: 'Gym Class Heros',
+      genre: 'Pop',
+      song: 'Stereo Hearts',
+      image: "https://images-na.ssl-images-amazon.com/images/I/61uoghXnjNL._SS500.jpg"
+  },
+  {
+      artist: 'Maroon 5',
+      genre: 'Pop',
+      song: 'Payphone',
+      image: "https://is2-ssl.mzstatic.com/image/thumb/Music/v4/52/b9/e8/52b9e8fd-1d04-ed0a-2c00-f4ebe25d769f/UMG_cvrart_00602537109708_01_RGB72_1200x1200_12UMGIM26179.jpg/268x0w.jpg"
+  },
+  {
+      artist: 'Rixton',
+      genre: 'Pop',
+      song: 'Me and My Brocken Heart',
+      image: "https://images.genius.com/35bd97138f1e737653165e5de8c7fdbe.1000x1000x1.jpg"
+  },
+  {
+      artist: 'Black Eyed Peas',
+      genre: 'Pop',
+      song: "I've Got a Feeling",
+      image: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f4/I_Gotta_Feeling.png/220px-I_Gotta_Feeling.png"
+  },
+  {
+      artist: 'AJR',
+      genre: 'Pop',
+      song: 'Sober Up',
+      image: "https://i.ytimg.com/vi/3qce2Z4nlVs/maxresdefault.jpg"
+  },
+///----------------------------------------2
+{
+    artist: 'The Beatles',
+    genre: 'Rock',
+    song: 'Ob-la-di Ob-la-da',
+    image: "https://images-na.ssl-images-amazon.com/images/I/316GrhxGleL._SY355_.jpg"
+  },
+  {
+      artist: 'Queen',
+      genre: 'Rock',
+      song: 'Another One Bites the Dust',
+      image: "https://upload.wikimedia.org/wikipedia/en/thumb/4/4d/Another_one_bites_the_dust.jpg/220px-Another_one_bites_the_dust.jpg"
+  },
+  {
+      artist: 'Jay and the Americans',
+      genre: 'Rock',
+      song: 'Come a Little Bit Closer',
+      image: "https://img.discogs.com/roBC2KZDDsYZ71PRqI-Dj3WxTxU=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-5986619-1491047673-1454.png.jpg"
+  },
+  {
+      artist: 'Blue Swede',
+      genre: 'Rock',
+      song: 'Hooked on a Feeling',
+      image: "https://upload.wikimedia.org/wikipedia/en/thumb/6/62/Hooked_on_a_Feeling_-_Blue_Swede.jpg/220px-Hooked_on_a_Feeling_-_Blue_Swede.jpg"
+  },
+  {
+      artist: 'Tom Petty',
+      genre: 'Rock',
+      song: 'Free Falling',
+      image: "https://images.rapgenius.com/b3ea1bb76509b8c29cb2eb4009e1e0ea.220x223x1.jpg"
+  },
+  {
+      artist: 'Survivor',
+      genre: 'Rock',
+      song: 'Eye of The Tiger',
+      image: "https://upload.wikimedia.org/wikipedia/en/8/8e/Eye_of_the_Tiger_Survivor.jpg"
+  },
+///----------------------------------------3
+{
+    artist: '',
+    genre: '',
+    song: '',
+    image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+///----------------------------------------4
+{
+    artist: '',
+    genre: '',
+    song: '',
+    image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+///----------------------------------------5
+{
+    artist: '',
+    genre: '',
+    song: '',
+    image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+///----------------------------------------6
+{
+    artist: '',
+    genre: '',
+    song: '',
+    image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+  {
+      artist: '',
+      genre: '',
+      song: '',
+      image: ""
+  },
+];
